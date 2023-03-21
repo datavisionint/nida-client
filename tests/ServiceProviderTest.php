c39c7d1159ce5debffdb9acf502ceb5c72397ab1
John Mwakalinga <mwakalingajohn@gmail.com>
2023-03-21 16:42:35 +0300

    removes migration test

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -48,37 +48,6 @@ class ServiceProviderTest extends TestCase
         static::assertFileEquals(base_path('config/nida-client.php'), __DIR__ . '/../config/nida-client.php');
     }
 
-    public function test_publishes_migrations(): void
-    {
-        $this->filesystem->ensureDirectoryExists(database_path('migrations'));
-
-        $this->artisan(
-            'vendor:publish',
-            [
-                '--provider' => 'SoftwareGalaxy\NidaClient\NidaClientServiceProvider',
-                '--tag' => 'migrations',
-            ]
-        )->run();
-
-        $files = collect($this->filesystem->files($this->app->databasePath('migrations')));
-
-        static::assertTrue(
-            $files->contains(
-                static function (SplFileInfo $file): bool {
-                    return preg_match('/.+\d{4}_\d{2}_\d{2}_\d{6}_(create_user_settings_table.php)$/', $file->getPathname());
-                }
-            )
-        );
-
-        static::assertTrue(
-            $files->contains(
-                static function (SplFileInfo $file): bool {
-                    return preg_match('/.+\d{4}_\d{2}_\d{2}_\d{6}_(create_user_settings_metadata_table.php)$/', $file->getPathname());
-                }
-            )
-        );
-    }
-
     protected function tearDown(): void
     {
         $this->filesystem->delete(base_path('config/nida-client.php'));
